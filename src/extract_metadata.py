9950e8121ba92304edbcfadb06551f467b46e27e
Sadhana Dubey <107179228+sadhanadby@users.noreply.github.com>
2025-03-30 15:19:17 +0530

    Added extract_metadata.py for audio data project

--- src/extract_metadata.py
+++ src/extract_metadata.py
@@ -1,1 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas
+import numpy
+import librosa
+
+
+# In[ ]:
+
+
+get_ipython().system('pip install librosa')
+
+
+# In[ ]:
+
+
+import librosa
+print(librosa.__version__)
+
+
+# In[5]:
+
+
+import os
+import pandas as pd
+
+# Path to genres folder
+base_path = 'data\genres_original'
+data = []
+
+# Loop through genres and extract file details
+for genre in os.listdir(base_path):
+    genre_path = os.path.join(base_path, genre)
+    if os.path.isdir(genre_path):
+        for file_name in os.listdir(genre_path):
+            file_path = os.path.join(genre_path, file_name)
+            data.append([file_name, genre, file_path])
+
+# Create metadata DataFrame
+df = pd.DataFrame(data, columns=["TrackName", "Genre", "FilePath"])
+df.to_csv('data/metadata.csv', index=False)
+print("✅ Metadata saved as data/metadata.csv")
 
